39934ea7bab0db2ccde2620451961968c7e99a3a
YAMASAKI Masahide <masahide.y@gmail.com>
2021-12-28 23:47:48 +0900

    Initial commit

---
+++ test/discord-bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DiscordBot from '../lib/discord-bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/discord-bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DiscordBot.DiscordBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
